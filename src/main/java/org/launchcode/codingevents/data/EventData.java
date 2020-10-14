e8ff82509829ad266b95d4acb73f46b194d110d2
Liza Baez Kemp <baezkemp@gmail.com>
2020-10-13 20:43:33 -0500

    13.3 Completed

--- src/main/java/org/launchcode/codingevents/data/EventData.java
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -7,22 +7,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EventData {
-
-    public static final Map<Integer, Event> events = new HashMap<>();
-
+//Place to put events
+    private static final Map<Integer, Event> events = new HashMap<>();
+//get all events
     public static Collection<Event> getAll(){
         return events.values();
     }
-
+// get a single event
     public static Event getById(int id){
         return events.get(id);
     }
-
+//add an event
     public static void add(Event event){
         events.put(event.getId(), event);
     }
-
-    public static void remove(Integer id) {
+//delete an event
+    public static void remove(int id) {
         if (events.containsKey(id)) {
             events.remove(id);
         }
